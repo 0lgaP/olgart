2b904e70883c26ca8d9fe7b1d74e82aa47fb2ac4
0lgaP <perronolga@gmail.com>
2024-07-24 16:21:25 -0400

    took out the flip effect as sepporate component

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,32 +1,22 @@
 import React, { useState } from "react";
 import "./about.css";
-import olga from "../../assets/bluehair.png";
-import olga_head from "../../assets/olga_head.png";
+import olga from "../../assets/bluehair.png"; /* change cartoon */
+import olga_head from "../../assets/olga_head.png"; /* change photo */
+
+import Flip from './Flip'
 
 const About = () => {
   const [over, setOver] = useState(false);
   return (
     <section id="about">
-      <div className="about__container">
-        <div
-          className="about__me"
-          onMouseOver={() => setOver(true)}
-          onMouseOut={() => setOver(false)}
-        >
-          <img
-            className={over ? "about__me-image" : "about__me-cartoon"}
-            src={over ? olga_head : olga}
-            alt=""
-          />
-        </div>
         <div className="about__content">
+     <Flip one={olga} two={olga_head} alt={"blue girl cartoon turns into my smily freckled face"} />
           <p>
           Howdy ya’ll, 
 I am AlienToronto, an artist specializing in acrylic paintings which inspect the Toronto-coded similarities between mundane and cosmic. I'm looking to paint up your wall, garage or fence real nice! This summer I am looking to take my art to the public, and if you are here  score I'm nailing it so far!
 
           </p>
         </div>
-      </div>
     </section>
   );
 };
---
+++ src/components/about/Flip.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import "./Flip.css"; /* change all css related to flip */
+
+/* section id="about" */
+
+const Flip = ({ one, two, alt }) => {
+    const [over, setOver] = useState(false);
+    return (
+            <div className="flip__container">
+                <div
+                    className="flip__me"
+                    onMouseOver={() => setOver(true)}
+                    onMouseOut={() => setOver(false)}
+                >
+                    <img
+                        className={over ? "flip__me-image" : "flip__me-cartoon"}
+                        src={over ? two : one}
+                        alt={alt && "on closer inspection this item is somehting else, this is a lil eye trick i built in, not really alt worthy, but ig you are here hai!"}
+                    />
+                </div>
+            </div>
+    );
+};
+
+export default Flip;
