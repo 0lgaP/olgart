21be0f0bf29642ab7a9630fae2b1eba8d10435a2
0lgaP <perronolga@gmail.com>
2025-08-17 12:16:22 -0400

    colour fic, info change

--- src/components/Publications/Public.jsx
+++ src/components/Publications/Public.jsx
@@ -9,34 +9,39 @@ const Public = () => {
     return (
         <section id="public">
             <div className="public__container">
-          
+
                 <div className=" public__card">
-          
+
                     <img
                         className={"public__me-image"}
-                        src={ olga_head }
+                        src={olga_head}
                         alt=""
                     />
-<div className="public__textbox">
-    
-                    <h3>
-                    🌹 Auburn Lane 
-                    </h3>
-                    <p>
-                        This wonderful female lead publication catured some deep Alien Toronto lore. If you even wondered..."why?", this will answer some but not all of your questions! 
-                        <br></br> 
-                    </p>
-                    <div className="public__link">
-                    <a href="https://www.auburnlane.com/alien-toronto-offers-cosmic-tarot-readings-at-buskerfest/"
-                      target="_blank"
-                      rel="noreferrer"
-                      aria-label="new tab to Aubirn Lane article"
-                    >Take me there, I am a reader! ⚡ </a>
-                    </div>
+                    <div className="public__textbox">
+
+                        <h3>
+                            🌹 Auburn Lane
+                        </h3>
+                        <p>
+                            This wonderful female-lead publication catured some deep Alien Toronto lore. If you even wondered..."why?", this will answer some but not all of your questions!
+                            <br></br>
+                        </p>
+
+                        <a href="https://www.auburnlane.com/alien-toronto-offers-cosmic-tarot-readings-at-buskerfest/"
+                            className="public__link"
+                            target="_blank"
+                            rel="noreferrer"
+                            aria-label="new tab to Aubirn Lane article"
+                        >
+                            <div >
+                                Take me there, I am a reader! ⚡
+                            </div>
+                        </a>
                     </div>
                 </div>
             </div>
-        </section>
+    
+        </section >
     );
 };
 
